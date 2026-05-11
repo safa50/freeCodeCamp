68650c871eaa5ece2372539806e2d7f28e81e7e9
Safa Dundar <safa.50@live.nl>
2026-05-11 10:06:21 +0000

    WIP

--- Python Certification/Workshops/email_simulator.py
+++ Python Certification/Workshops/email_simulator.py
@@ -8,3 +8,15 @@ class Email:
 
     def mark_as_read(self):
         self.read = True
+
+class User:
+    def __init__(self, name, inbox):
+        self.name = name
+        self.inbox = []
+
+    def send_email(self, receiver, subject, body):
+        email = Email(sender=self, receiver=receiver, subject=subject, body=body)
+
+class Inbox:
+    def __init__(self):
+        self.emails = []
