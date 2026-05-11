fca46536ab87a30efefcb918123dc8b53dca99de
Safa Dundar <safa.50@live.nl>
2026-05-11 10:41:39 +0000

    further updates

--- Python Certification/Workshops/email_simulator.py
+++ Python Certification/Workshops/email_simulator.py
@@ -1,3 +1,5 @@
+import datetime
+
 class Email:
     def __init__(self, sender, receiver, subject, body):
         self.sender = sender
@@ -9,14 +11,68 @@ class Email:
     def mark_as_read(self):
         self.read = True
 
+    def display_full_email(self):
+        self.mark_as_read()
+        print('\n--- Email ---')
+        print(f'From: {self.sender.name}')
+        print(f'To: {self.receiver.name}')
+        print(f'Subject: {self.subject}')
+        print(f'Body: {self.body}')
+        print('------------\n')
+
+    def __str__(self):
+        status = "Read" if self.read else "Unread"
+        return f'[{status}] From: {self.sender.name} | Subject: {self.subject}'
+
 class User:
-    def __init__(self, name, inbox):
+    def __init__(self, name):
         self.name = name
-        self.inbox = []
+        self.inbox = Inbox()
 
     def send_email(self, receiver, subject, body):
         email = Email(sender=self, receiver=receiver, subject=subject, body=body)
+        receiver.inbox.receive_email(email)
 
 class Inbox:
     def __init__(self):
         self.emails = []
+    
+    def receive_email(self, email):
+        self.emails.append(email)
+
+    def list_emails(self):
+        if not self.emails:
+            print("Your inbox is empty. \n")
+            return
+        
+        print("\nYour Emails:")
+        for i, email in enumerate(self.emails, start=1):
+            print(f"{i}. {email}")
+
+    def read_email(self, index):
+        if not self.emails:
+            print('Inbox is empty.\n')
+            return
+        actual_index = index - 1
+        if actual_index < 0 or actual_index >= len(self.emails):
+            print('Invalid email number.\n')
+            return
+        self.emails[actual_index].display_full_email()
+    
+    def delete_email(self, index):
+        if not self.emails:
+            print('Inbox is empty.\n')
+            return
+        actual_index = index - 1
+        if actual_index < 0 or actual_index >= len(self.emails):
+            print('Invalid email number.\n')
+            return
+        del self.emails[actual_index]
+        print('Email deleted.\n')
+'''
+alice = User("Alice")
+bob = User("Bob")
+print(len(bob.inbox.emails))  # Output: 0
+alice.send_email(bob, "Hello", "Hi Bob, how are you?")
+print(len(bob.inbox.emails))  # Output: 1
+'''
